fa6120b8222375174c5744327cf047d6929d05e7
sadio1 <sadio.faye.maguette.ndaiye@gmail.com>
2024-05-06 10:11:17 +0000

    first back

---
+++ Back/database/migrations/2024_04_30_183910_create_departements_table.php
@@ -0,0 +1,31 @@
+<?php
+use App\Models\Direction;
+use App\Models\Pole;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id();
+            $table->string('libelle');
+            $table->foreignIdFor(Direction::class)->constrained();
+            $table->foreignIdFor(Pole::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departements');
+    }
+};
