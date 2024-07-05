edbe4477cab5e4a82f6eb0e9762ed8f2a49fa976
sadio1 <sadio.faye.maguette.ndaiye@gmail.com>
2024-07-05 08:48:59 +0000

    vendredicommit

--- Frontb/src/app/home/profil/profil.component.ts
+++ Frontb/src/app/home/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profil',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './profil.component.css'
 })
 export class ProfilComponent {
+  titre!: string
+  btn!: string
+  profilForm!:FormGroup;
+
+  openModal()
+  {
+    let modal = document.getElementById('land');
+    if (modal) {
+      this.titre = 'Nouveau Pays'
+      this.btn = 'Ajouter'
+      modal.style.display = 'block';
+    }
+  }
+  
+  closeModal()
+  {
+    let modal = document.getElementById('land');
+    if (modal) {
+      modal.style.display = 'none';
+      this.profilForm.reset()
+    }
+  }
 
 }
